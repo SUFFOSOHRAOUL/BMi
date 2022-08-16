e72c9f4080e1846d6bfe0f725f2785ef506aed33
unknown <sohraoulsuffo@gmail.com>
2022-08-16 12:25:11 +0100

    creating independant files for the reuseable card and icon content

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
+import 'reuseable_card.dart';
+import 'icon_content.dart';
 const   bottomContainerHeight = 80.0;
 const  cardcolor = Color(0xff1d1e33);
 const bottomContainerColor = Color(0xffeb1555);
@@ -82,43 +83,3 @@ class inputPage extends StatelessWidget {
   }
 }
 
-class topCardUI extends StatelessWidget {
-  topCardUI({required this.icon,required this.label});
-  final IconData icon;
-  final String label;
-  @override
-  Widget build(BuildContext context) {
-    return Column (
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget> [
-          Icon(icon, size: 80),
-        SizedBox(
-          height: 15,
-        ),
-          Text(
-            label ,style: TextStyle(
-             color: Color(0xff8e8e98),
-              fontSize: 18.0, 
-            ),
-          )
-      ],
-    );
-  }
-}
-
-class ReuseableCard extends StatelessWidget {
-  ReuseableCard({required this.colour , required this.cardChild});
-     final   Color colour;
-     final Widget cardChild ;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-                child: cardChild,
-                 margin: EdgeInsets.all(10.0),
-                 decoration:  BoxDecoration(
-                  color: colour,
-                  borderRadius: BorderRadius.circular(10.0),
-                 ),
-                );
-  }
-}
